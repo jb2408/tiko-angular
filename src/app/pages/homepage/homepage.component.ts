e281709097499035bdc063baf66dbf54648b4d13
Justyna Biel <justyna.biel@capgemini.com>
2023-02-16 22:37:35 +0100

    isUserKnow boolean + ngrx instalation

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  isUserKnow: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
